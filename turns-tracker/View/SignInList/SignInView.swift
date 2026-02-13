61aea32099b8be3a1aede44902a22d696a3ab2e2
Reshlynt <dobao98123@gmail.com>
2026-02-13 14:02:42 -0500

    Combine the views into SIgnInView for easy modification

---
+++ turns-tracker/View/SignInList/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  turns-tracker
+//
+//  Created by Scott Do on 2/13/26.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
